e9504d5967ab5e332e9be59d4dc6e300ba724fbd
GeorgeRidgeway <109179341+GeorgeRidgeway@users.noreply.github.com>
2023-11-14 09:10:04 -0500

    Update script_portfolio.js

--- script_portfolio.js
+++ script_portfolio.js
@@ -1,63 +1,63 @@
 
 
-var countElStrikes = document.getElementById("count-el-strikes")
+var countElStrikes = document.getElementById("count-el-strikes");
 
-var countElBalls = document.getElementById("count-el-balls")
+var countElBalls = document.getElementById("count-el-balls");
 
-var countElBatter = document.getElementById("count-el-batter")
+var countElBatter = document.getElementById("count-el-batter");
 
-var countElResult = document.getElementById("count-el-previous")
+var countElResult = document.getElementById("count-el-previous");
 
-var countStrikes = 0
+var countStrikes = 0;
 
-var countBalls = 0
+var countBalls = 0;
 
-var count = 0
+var count = 0;
 
 
 function strikes() {
 	if (countElBatter.innerText !== "Walk") {
-  		countStrikes += 1
-  		countElStrikes.innerText = countStrikes
+  		countStrikes += 1;
+  		countElStrikes.innerText = countStrikes;
 	}
   	if (countStrikes == 3) {
-  		countElBatter.innerText = "Out"
+  		countElBatter.innerText = "Out";
 	}
 	if(countStrikes >= 4) {
     		countStrikes = 0;
     		countBalls = 0;
     		document.getElementById("count-el-strikes").innerHTML = countStrikes; 
     		document.getElementById("count-el-balls").innerHTML = countBalls; 
-    		countElBatter.innerText = ""
+    		countElBatter.innerText = "";
    	}
 } 
 
 function balls() {
 	if (countElBatter.innerText !== "Out") {
 		countBalls += 1;
-		countElBalls.innerText = countBalls
+		countElBalls.innerText = countBalls;
 	}
   	if (countBalls == 4) {
-  		countElBatter.innerText = "Walk"
+  		countElBatter.innerText = "Walk";
  	}
   	if(countBalls >= 5) {
   		countStrikes = 0;
 		countBalls = 0;
   		document.getElementById("count-el-strikes").innerHTML = countStrikes; 
   		document.getElementById("count-el-balls").innerHTML = countBalls; 
-    		countElBatter.innerText = ""
+    		countElBatter.innerText = "";
 	}
 }
 
 function save(){
-	result = countElBatter.innerText
+	result = countElBatter.innerText;
 	if ( result == "Walk" || result == "Out" ) {
-    		countElResult.innerText += result + " - "
+    		countElResult.innerText += result + " - ";
     		countStrikes = 0;
 		countBalls = 0;
   		document.getElementById("count-el-strikes").innerHTML = countStrikes; 
   		document.getElementById("count-el-balls").innerHTML = countBalls;
-  		countElBatter.innerText = ""
+  		countElBatter.innerText = "";
   	}
 }
 
@@ -68,5 +68,5 @@ function reset() {
   countElResult.innerText = " ";
   document.getElementById("count-el-strikes").innerHTML = countStrikes; 
   document.getElementById("count-el-balls").innerHTML = countBalls;
-  countElBatter.innerText = ""
+  countElBatter.innerText = "";
 }
